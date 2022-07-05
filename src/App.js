eb6236b29abdcf035c982a087a1f14527b55636d
Jorge Roldán <icabina@live.com>
2022-07-05 18:43:02 -0500

    m

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import ButtonChange from "./ButtonChange";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SummaryForm from "./pages/summary/SummaryForm";
-import Options from "./pages/entry/Optons";
+import OrderEntry from "./pages/entry/OrderEntry";
 
+import Container from "react-bootstrap/Container";
+
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
+
+import OrderSummary from "./pages/summary/OrderSummary";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 //npm install eslint-plugin-testing-library eslint-plugin-jest
 
-function App() {
+export default function App() {
+  // orderPhase needs to be 'inProgress', 'review' or 'completed'
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+
+  let Component = OrderEntry; // default to order page
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
-    <div>
+    <OrderDetailsProvider>
       <ButtonChange />
       <SummaryForm />
-      <Options />
-    </div>
+      <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
+    </OrderDetailsProvider>
   );
 }
-
-export default App;
